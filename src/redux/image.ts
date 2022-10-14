b0b15645cf4bd278bb53911651add4070042c046
lnuvy <lnuvy.code@gmail.com>
2022-10-14 12:03:25 +0900

    check

--- src/redux/image.ts
+++ src/redux/image.ts
@@ -1,28 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   preview: null,
   imageArr: [],
-}
+};
 
 const imageSlice = createSlice({
-  name: 'image',
+  name: "image",
   initialState,
   reducers: {
     upload: (state, action) => {
-      state.imageArr.push(action.payload)
+      state.imageArr.push(action.payload);
     },
 
     removePreview: (state, action) => {
       state.imageArr = state.imageArr.filter((el) => {
-        return el !== action.payload
-      })
+        return el !== action.payload;
+      });
     },
   },
-})
+});
 
-const { reducer, actions } = imageSlice
+const { reducer, actions } = imageSlice;
 
-export const { accrueImage } = actions
+export const { upload, removePreview } = actions;
 
-export default reducer
+export default reducer;
